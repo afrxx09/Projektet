d51be5b2e8791109ea540f2b0bd2a5faf8772d41
Andreas Fridlund <afrxx09@student.lnu.se>
2013-10-29 08:56:51 -0400

    commit för att göra speed-test mot github. cloud 9 är för labilt...

--- js/form.js
+++ js/form.js
@@ -1,30 +1,43 @@
 var form = {
     strSelected : 0,
     
-    Init : function(strSelected){
+    Init : function(strSelected, boolSend){
         this.Bind();
-        if(strSelected !== 0){
-            this.strSelected = strSelected;
+        if(boolSend){
+            $('#form-container').hide();
+            $('#form-submitted').show();
         }
-        this.SelectProduct();
+        else{
+            if(strSelected !== 0){
+                this.strSelected = strSelected;
+            }
+            this.SelectProduct();
+        }
     },
     
     Bind : function(){
         var self = this;
+        $('#submit-button').on('click',function(){
+            $('#order-form').trigger('submit');
+        });
     },
     
     SelectProduct : function(){
         $('#product').val(this.strSelected);
-        console.log(this.strSelected);
     }
 };
 
 $(document).ready(function(){
-    var strURL = document.URL;
-    var intIndex = strURL.indexOf('?');
+    var strURL      = document.URL;
+    var intIndex    = strURL.indexOf('?');
     var intSelected = 0;
     if(intIndex !== -1){
-        intSelected = parseInt(strURL.substring(intIndex+1));
+        var strParam = strURL.substring(intIndex+1);
+        if(strParam === 'send'){
+            form.Init(intSelected, true);
+        }
+        else{
+            form.Init(parseInt(strParam), false);
+        }
     }
-    form.Init(intSelected);
 });
